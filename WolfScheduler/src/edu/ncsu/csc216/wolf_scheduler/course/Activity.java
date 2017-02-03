032a1afbf2733735d98b7d2358d28ec456b97f7d
Caitlyn <Caitlyn@Caitlyns-Laptop>
2017-02-03 12:01:40 -0500

    Changed .equals to .contains

--- WolfScheduler/src/edu/ncsu/csc216/wolf_scheduler/course/Activity.java
+++ WolfScheduler/src/edu/ncsu/csc216/wolf_scheduler/course/Activity.java
@@ -263,7 +263,7 @@ public abstract class Activity implements Conflict {
 	 */
 	@Override
 	public void checkConflict(Activity possibleConflictingActivity) throws ConflictException {
-		if (!this.getMeetingDays().equalsIgnoreCase("Arranged") && !possibleConflictingActivity.getMeetingDays().equalsIgnoreCase("Arranged")) {
+		if (!this.getMeetingDays().contains("Arranged") && !possibleConflictingActivity.getMeetingDays().contains("Arranged")) {
 			String longDays = "";
 			String shortDays = "";
 			if (this.getMeetingDays().length() > possibleConflictingActivity.getMeetingDays().length()) {
